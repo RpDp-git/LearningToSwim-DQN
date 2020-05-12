4f8acc705c44f313b4afc77f3bb258c8c5736b86
Ravi Pradip <ravipradip@gmail.com>
2020-05-12 19:27:58 +0200

    Update learn.py

--- learn.py
+++ learn.py
@@ -5,7 +5,7 @@ render=False
 save=False
 agent=DQN.Pw_Agent(gamma=0.995,epsilon=1.0)
 for i in range(600):
-    if i%5==0 :
+    if i%50==0 :
         render=True
     else:
         render=False
@@ -13,6 +13,6 @@ for i in range(600):
         torch.save(agent.policy_NN.state_dict(), 'episode {}'.format(i))
 
 
-    score=agent.DQNepisode(N_steps=10000,vid=render)
+    score,reason=agent.DQNepisode(N_steps=10000,vid=render)
     scores.append(score)
-    print("Episode ",i,"Score ",score)
+    print("Episode ",i,"Score ",score,'Status :',reason)
