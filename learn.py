b377e705b270566052584196672f9d7998b2f78a
Ravi Pradip <ravipradip@gmail.com>
2020-05-19 09:15:52 +0200

    Tensorboard logging

--- learn.py
+++ learn.py
@@ -1,18 +1,34 @@
 import DQN
 import torch
+from torch.utils.tensorboard import SummaryWriter
+
+vic=[]
 scores=[]
 render=False
 save=False
-agent=DQN.Pw_Agent(gamma=0.995,epsilon=1.0)
-for i in range(600):
+agent=DQN.Pw_Agent(gamma=0.93,epsilon=1.0)
+tb = SummaryWriter(comment=agent.get_hyperparams())
+print(agent.get_hyperparams())
+print()
+for i in range(400):
     if i%50==0 :
-        render=True
+        render=False
     else:
         render=False
     if i%50==0:
         torch.save(agent.policy_NN.state_dict(), 'episode {}'.format(i))
 
 
-    score,reason=agent.DQNepisode(N_steps=10000,vid=render)
+    reason, score,loss = agent.DQNepisode(N_steps=10000,vid=render)
     scores.append(score)
-    print("Episode ",i,"Score ",score,'Status :',reason)
+    print("Episode ",i,"Score ",score,'Status: ',reason)
+    vic.append(reason)
+    if len(vic)>50:
+        a=vic[-50:]
+        my_dict = {i:a.count(i) for i in a}
+        suc=int(my_dict['GOAL'])/50 * 100
+        print('success rate = ', suc)
+        tb.add_scalar('Success Rate', suc, i)
+        tb.flush()
+
+tb.close()
